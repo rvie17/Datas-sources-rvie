c39487f45030aa1fd28c141fad42b58ff1143943
rvie <rvie@mail.com>
2022-11-29 15:23:07 +0100

    firt commit

---
+++ first_micro/main.py
@@ -0,0 +1,18 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/', methods=["GET"])
+def hello_world():
+ prefix_google = """
+ <!-- Google tag (gtag.js) -->
+<script async
+src="https://www.googletagmanager.com/gtag/js?id=G-YBSGVH69GD"></script>
+<script>
+ window.dataLayer = window.dataLayer || [];
+ function gtag(){dataLayer.push(arguments);}
+ gtag('js', new Date());
+ gtag('config', ' G-YBSGVH69GD');
+</script>
+ """
+ return prefix_google + "Hello World"
