01bbf57b141bdce482adcd091b6e577fef7a63bb
rvie <rvie@mail.com>
2022-12-06 14:36:28 +0100

    Adding minimalist logger

--- first_micro/main.py
+++ first_micro/main.py
@@ -1,6 +1,9 @@
 from flask import Flask
+import logging
 
+logging.basicConfig(filename='record.log', level=logging.DEBUG)
 app = Flask(__name__)
+
 
 @app.route('/', methods=["GET"])
 def hello_world():
@@ -16,3 +19,11 @@ src="https://www.googletagmanager.com/gtag/js?id=G-YBSGVH69GD"></script>
 </script>
  """
  return prefix_google + "Hello World"
+
+@app.route('/logger', methods=["GET"])
+def hello_world2():
+    print("I'm a logger")
+    #app.logger.debug("Debug log level")
+    return "I'm a logger"
+if __name__ == '__main__':
+  app.run(debug=True)
